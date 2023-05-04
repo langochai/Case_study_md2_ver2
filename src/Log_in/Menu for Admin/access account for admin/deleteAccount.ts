e3b6028507dea39b7f2c44ef413ca4fdba0a8052
langochai <langochai11121998@gmail.com>
2023-05-04 15:06:23 +0700

    update

--- src/Log_in/Menu for Admin/access account for admin/deleteAccount.ts
+++ src/Log_in/Menu for Admin/access account for admin/deleteAccount.ts
@@ -1,8 +1,14 @@
 import {AccountManager} from "../../../AccountManager";
 import {menuForAdmin} from "../menuForAdmin";
+import {readline} from "../../../../main";
 
 export function deleteAccount(username,list:AccountManager,checkUsername){
-    list.delete(checkUsername)
-    console.log(`Deleted. Return to admin menu.`)
+    console.log(`Do you really wanna delete this account?`)
+    if (readline.keyInYN('Yes / No ?')) {
+        list.delete(checkUsername)
+        console.log(`Deleted. Return to admin menu.`)
+    } else {
+        console.log(`No change was made. Return to admin menu`)
+    }
     menuForAdmin(username,list)
 }
